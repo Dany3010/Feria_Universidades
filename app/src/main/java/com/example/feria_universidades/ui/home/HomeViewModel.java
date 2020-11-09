24bc189c151e0c2ce2f3c715ae1f3426e050d1bd
Dany3010 <201800081@estudiantes.upqroo.edu.mx>
2020-11-09 17:58:39 -0500

    test

--- app/src/main/java/com/example/feria_universidades/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/feria_universidades/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("no hay nada que ver");
     }
 
     public LiveData<String> getText() {
