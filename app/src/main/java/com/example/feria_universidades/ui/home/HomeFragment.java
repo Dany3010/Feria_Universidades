35990570f67d953ce05c5d49e6ec28a410d0fe31
Dany3010 <201800081@estudiantes.upqroo.edu.mx>
2020-11-09 18:46:26 -0500

    diseño de home

--- app/src/main/java/com/example/feria_universidades/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/feria_universidades/ui/home/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.feria_universidades.ui.home;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +20,7 @@ import com.example.feria_universidades.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private VideoView videoView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,10 +28,19 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+        videoView=(VideoView)root.findViewById(R.id.video);
+        videoView.setVideoURI(Uri.parse("https://rentaclassic.000webhostapp.com/videos/secretaria.mp4"));
+
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+
+                MediaController mediaController = new MediaController(getActivity());
+                videoView.setMediaController(mediaController);
+                videoView.requestFocus();
+                videoView.start();
             }
         });
         return root;
