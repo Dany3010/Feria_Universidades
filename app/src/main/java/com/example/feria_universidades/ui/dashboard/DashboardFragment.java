c4c895a9b1fc1ee4960258e69bc5ae59fc7c8bdf
Dany3010 <201800081@estudiantes.upqroo.edu.mx>
2020-11-10 12:21:53 -0500

    diseño v2

--- app/src/main/java/com/example/feria_universidades/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/feria_universidades/ui/dashboard/DashboardFragment.java
@@ -1,10 +1,16 @@
 package com.example.feria_universidades.ui.dashboard;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,19 +23,32 @@ import com.example.feria_universidades.R;
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
-
+private WebView webView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
+        webView=(WebView)root.findViewById(R.id.webview);
+
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+                webView.setWebViewClient(new WebViewClient() { public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                   if (url.startsWith("vnd.youtube:")) {
+                        int n = url.indexOf("?"); if (n > 0) {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("https://www.youtube.com/watch?v=q8zuQ295U5s",
+                                    url.substring("vnd.youtube:".length(),n)))));
+                        } return (true);
+                    } return (false);
+                } });
+
+
             }
         });
         return root;
     }
-}
+
+    }
