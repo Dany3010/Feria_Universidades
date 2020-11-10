98779e5e31e86f1a9a918b9be1d79bba1cc7ab1e
Dany3010 <201800081@estudiantes.upqroo.edu.mx>
2020-11-10 08:44:31 -0500

    diseño de home v3

--- app/src/main/java/com/example/feria_universidades/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/feria_universidades/ui/home/HomeFragment.java
@@ -21,13 +21,14 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private VideoView videoView;
-
+    private TextView inicio;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+          inicio=(TextView)root.findViewById(R.id.parrafo1);
         videoView=(VideoView)root.findViewById(R.id.video);
         videoView.setVideoURI(Uri.parse("https://rentaclassic.000webhostapp.com/videos/secretaria.mp4"));
 
@@ -36,12 +37,16 @@ public class HomeFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
-
+                String p1="La Feria Virtual Universitaria es un evento estatal que reúne a " +
+                        "todos los Programas Educativos de nivel superior, en un esfuerzo por acercar la " +
+                        "Oferta Educativa a todos los jóvenes que quieren continuar su formación cursando una " +
+                        "carrera universitaria.";
+                inicio.setText(p1);
                 MediaController mediaController = new MediaController(getActivity());
                 mediaController.setAnchorView(videoView);
                 videoView.setMediaController(mediaController);
                 videoView.requestFocus();
-                videoView.start();
+
             }
         });
         return root;
