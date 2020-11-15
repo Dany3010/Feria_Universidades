723c973788125f854fc586ae6fe6f80691295866
Dany3010 <201800081@estudiantes.upqroo.edu.mx>
2020-11-15 15:51:21 -0500

    universidades

--- app/src/main/java/com/example/feria_universidades/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/feria_universidades/ui/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Notificaciones");
+        mText.setValue("");
     }
 
     public LiveData<String> getText() {
