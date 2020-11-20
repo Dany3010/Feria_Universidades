200291b0fef53ef2927db35d20e6a9822747061c
yosma <yosmarherrera000@gmail.com>
2020-11-20 15:36:53 -0500

    universidad detalle v2

--- app/src/main/java/com/example/feria_universidades/Uni_detalle.java
+++ app/src/main/java/com/example/feria_universidades/Uni_detalle.java
@@ -3,12 +3,137 @@ package com.example.feria_universidades;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Uni_detalle extends AppCompatActivity {
-
+  ImageView imagen_uni;
+  TextView recibidor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uni_detalle);
+
+        Bundle bundle = getIntent().getExtras();
+        String valor =bundle.getString("uni");
+        imagen_uni = (ImageView)findViewById(R.id.imagenUni);
+        recibidor= (TextView) findViewById(R.id.titulo_uni);
+
+        /////OTHON P. BLANCO////////
+        if (valor.equals("uni_itzm")){
+            recibidor.setText("Instituto Tecnológico de la Zona Maya");
+            imagen_uni.setImageResource(R.drawable.itzm);
+        }
+        if (valor.equals("uni_cudv")){
+            recibidor.setText("Centro Universitario de Desarrollo Vanguardista");
+            imagen_uni.setImageResource(R.drawable.cudv_1);
+        }
+        if (valor.equals("unid_chetumal")){
+            recibidor.setText("Universidad Interamericana para el  Desarrollo Campus Chetumal");
+            imagen_uni.setImageResource(R.drawable.unid_chetumal);
+        }
+        if (valor.equals("modelo_chetumal")){
+            recibidor.setText("Universidad Modelo");
+            imagen_uni.setImageResource(R.drawable.uni_modelo_chetumal);
+        }
+        if (valor.equals("uni_wozniak")){
+            recibidor.setText("Instituto de Estudios Superiores Wozniak");
+            imagen_uni.setImageResource(R.drawable.wozniak);
+        }
+        if (valor.equals("uqroo_chetumal")){
+            recibidor.setText("Universidad de Quintana Roo Campo Chetumal");
+            imagen_uni.setImageResource(R.drawable.uqroo);
+        }
+        if (valor.equals("ut_chetumal")){
+            recibidor.setText("Universidad Tecnológica de Chetumal");
+            imagen_uni.setImageResource(R.drawable.ut_chetumal);
+        }
+        ///////BACALAR/////////
+        if (valor.equals("politec_bacalar")){
+            recibidor.setText("Universidad Politécnica de Bacalar");
+            imagen_uni.setImageResource(R.drawable.uni_poli_bacalar);
+        }
+        if (valor.equals("centro_edu_bacalar")){
+            recibidor.setText("Centro Regional de Educación Normal Lic. Javier Rojo Gómez");
+            imagen_uni.setImageResource(R.drawable.centro_regio_edu_bacalar);
+        }
+        /////FELIPE CARRILLO PUERTO////////
+        if (valor.equals("upp")){
+            recibidor.setText("Universidad Privada de la Península");
+            imagen_uni.setImageResource(R.drawable.upp);
+        }
+        if (valor.equals("inst_tec_felipe_c")){
+            recibidor.setText("Instituto Tecnológico Superior de Felipe Carrillo Puerto");
+            imagen_uni.setImageResource(R.drawable.inst_tec_felipe_c);
+        }
+        /////JOSE MARIA MORELOS///////
+        if (valor.equals("uimqroo")){
+            recibidor.setText("Universidad Intercultural Maya de Quintana Roo");
+            imagen_uni.setImageResource(R.drawable.uimqroo);
+        }
+        /////SOLIDARIDAD///////
+        if (valor.equals("UniversidadSur_soli")){
+            recibidor.setText("Universidad del Sur Campus Playa del Carmen");
+            imagen_uni.setImageResource(R.drawable.universidadsur_solidaridad);
+        }
+        if (valor.equals("universidadriviera_soli")){
+            recibidor.setText("Universidad Riviera");
+            imagen_uni.setImageResource(R.drawable.universidadriviera_solidaridad);
+        }
+        if (valor.equals("ut_rivieramaya_soli")){
+            recibidor.setText("Universidad Tecnológica Riviera Maya");
+            imagen_uni.setImageResource(R.drawable.ut_rivieramaya_solidaridad);
+        }
+        if (valor.equals("unid_soli")){
+            recibidor.setText("Universidad Interamericana para el Desarrollo Campus Playa del Carmen");
+            imagen_uni.setImageResource(R.drawable.unid_solidaridad);
+        }
+        if (valor.equals("esc_enfermeria_soli")){
+            recibidor.setText("Escuela de enfermería Ignacio Chávez");
+            imagen_uni.setImageResource(R.drawable.esc_enfermeria_solidaridad);
+        }
+        /////COZUMEL///////
+        if (valor.equals("inst_partenon_cozumel")){
+            recibidor.setText("Instituto Partenón de Cozumel");
+            imagen_uni.setImageResource(R.drawable.inst_partenon_cozumel);
+        }
+
+        /////BENITO JUÁREZ///////
+        if (valor.equals("upqroo_benito_juarez")){
+            recibidor.setText("Universidad Politécnica de Quintana Roo");
+            imagen_uni.setImageResource(R.drawable.upqroo_benito_juarez);
+        }
+        if (valor.equals("ut_benito_juarez")){
+            recibidor.setText("Universidad Tecnológica de Cancún");
+            imagen_uni.setImageResource(R.drawable.ut_benito_juarez);
+        }
+        if (valor.equals("uni_henbord_benito_juarez")){
+            recibidor.setText("Universidad Henbord");
+            imagen_uni.setImageResource(R.drawable.uni_henbord_benito_juarez);
+        }
+        if (valor.equals("uni_azteca_benito_juarez")){
+            recibidor.setText("Universidad Azteca");
+            imagen_uni.setImageResource(R.drawable.uni_azteca_benito_juarez);
+        }
+        if (valor.equals("inst_humanista_gestalt_benito_juarez")){
+            recibidor.setText("Instituto Humanista de Psicoterapia Gestalt");
+            imagen_uni.setImageResource(R.drawable.inst_humanista_gestalt_benito_juarez);
+        }
+        if (valor.equals("la_salle_benito_juarez")){
+            recibidor.setText("La Salle Campus Cancún");
+            imagen_uni.setImageResource(R.drawable.la_salle_benito_juarez);
+        }
+        if (valor.equals("centro_gestalt_benito_juarez")){
+            recibidor.setText("Cento de Estudios Gestalt");
+            imagen_uni.setImageResource(R.drawable.centro_gestalt_benito_juarez);
+        }
+        if (valor.equals("unisur_benito_juarez")){
+            recibidor.setText("Universidad del Sur Campus Cancún");
+            imagen_uni.setImageResource(R.drawable.unisur_benito_juarez);
+        }
+        if (valor.equals("instituto_sup_leyes_benito_juarez")){
+            recibidor.setText("Instituto Superior de Leyes");
+            imagen_uni.setImageResource(R.drawable.instituto_sup_leyes_benito_juarez);
+        }
     }
 }
--- app/src/main/java/com/example/feria_universidades/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/feria_universidades/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,6 @@
 package com.example.feria_universidades.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +14,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.feria_universidades.MainActivity2;
 import com.example.feria_universidades.R;
+import com.example.feria_universidades.Uni_detalle;
 
 public class NotificationsFragment extends Fragment {
     /////OTHON P. BLANCO////////
@@ -46,17 +49,283 @@ Button uimqroo;
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
+
+        /////OTHON P. BLANCO////////
+        itzm =(Button)root.findViewById(R.id.itzm);
+        cudv =(Button)root.findViewById(R.id.cudv);
+        unid_chetumal =(Button)root.findViewById(R.id.unid_chetumal);
+        uni_modelo_chetumal =(Button)root.findViewById(R.id.uni_modelo_chetumal);
+        uni_wozniak =(Button)root.findViewById(R.id.uni_wozniak);
+        uqroo_chetumal =(Button)root.findViewById(R.id.uqroo_chetumal);
+        ut_chetumal =(Button)root.findViewById(R.id.ut_chetumal);
+
+        /////BACALAR////////
+        uni_poli_bacalar =(Button)root.findViewById(R.id.uni_poli_bacalar);
+        centro_regio_edu_bacalar =(Button)root.findViewById(R.id.centro_regio_edu_bacalar);
+
+        /////FELIPE CARRILLO PUERTO////////
+        upp =(Button)root.findViewById(R.id.upp);
+        inst_tec_felipe_c =(Button)root.findViewById(R.id.inst_tec_felipe_c);
+
+        /////JOSE MARIA MORELOS///////
+        uimqroo =(Button)root.findViewById(R.id.uimqroo);
+
+        /////SOLIDARIDAD///////
+        UniversidadSur_soli =(Button)root.findViewById(R.id.UniversidadSur_soli);
+        universidadriviera_soli =(Button)root.findViewById(R.id.universidadriviera_soli);
+        ut_rivieramaya_soli =(Button)root.findViewById(R.id.ut_rivieramaya_soli);
+        unid_soli =(Button)root.findViewById(R.id.unid_soli);
+        esc_enfermeria_soli =(Button)root.findViewById(R.id.esc_enfermeria_soli);
+
+        /////COZUMEL///////
+        inst_partenon_cozumel =(Button)root.findViewById(R.id.inst_partenon_cozumel);
+
+        /////BENITO JUÁREZ///////
+        upqroo_benito_juarez =(Button)root.findViewById(R.id.upqroo_benito_juarez);
+        ut_benito_juarez =(Button)root.findViewById(R.id.ut_benito_juarez);
+        uni_henbord_benito_juarez =(Button)root.findViewById(R.id.uni_henbord_benito_juarez);
+        uni_azteca_benito_juarez =(Button)root.findViewById(R.id.uni_azteca_benito_juarez);
+        centro_gestalt_benito_juarez =(Button)root.findViewById(R.id.centro_gestalt_benito_juarez);
+        la_salle_benito_juarez =(Button)root.findViewById(R.id.la_salle_benito_juarez);
+        inst_humanista_gestalt_benito_juarez =(Button)root.findViewById(R.id.inst_humanista_gestalt_benito_juarez);
+        unisur_benito_juarez =(Button)root.findViewById(R.id.unisur_benito_juarez);
+        instituto_sup_leyes_benito_juarez =(Button)root.findViewById(R.id.instituto_sup_leyes_benito_juarez);
+
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
                 /////OTHON P. BLANCO////////
+                itzm.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","uni_itzm");
+                        startActivity(univerisdad);
+                    }
+                });
+
+                cudv.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","uni_cudv");
+                        startActivity(univerisdad);
+                    }
+                });
+
+                unid_chetumal.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","unid_chetumal");
+                        startActivity(univerisdad);
+                    }
+                });
+
+                uni_modelo_chetumal.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","modelo_chetumal");
+                        startActivity(univerisdad);
+                    }
+                });
+
+                uni_wozniak.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","uni_wozniak");
+                        startActivity(univerisdad);
+                    }
+                });
+
+                uqroo_chetumal.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","uqroo_chetumal");
+                        startActivity(univerisdad);
+                    }
+                });
+
+                ut_chetumal.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","ut_chetumal");
+                        startActivity(univerisdad);
+                    }
+                });
                 ///////BACALAR/////////
+                uni_poli_bacalar.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","politec_bacalar");
+                        startActivity(univerisdad);
+                    }
+                });
+                centro_regio_edu_bacalar.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","centro_edu_bacalar");
+                        startActivity(univerisdad);
+                    }
+                });
                 /////FELIPE CARRILLO PUERTO////////
+                upp.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","upp");
+                        startActivity(univerisdad);
+                    }
+                });
+                inst_tec_felipe_c.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","inst_tec_felipe_c");
+                        startActivity(univerisdad);
+                    }
+                });
                 /////JOSE MARIA MORELOS///////
+                uimqroo.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","uimqroo");
+                        startActivity(univerisdad);
+                    }
+                });
+
                 /////SOLIDARIDAD///////
+                UniversidadSur_soli.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","UniversidadSur_soli");
+                        startActivity(univerisdad);
+                    }
+                });
+                universidadriviera_soli.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","universidadriviera_soli");
+                        startActivity(univerisdad);
+                    }
+                });
+                ut_rivieramaya_soli.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","ut_rivieramaya_soli");
+                        startActivity(univerisdad);
+                    }
+                });
+                unid_soli.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","unid_soli");
+                        startActivity(univerisdad);
+                    }
+                });
+                esc_enfermeria_soli.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","esc_enfermeria_soli");
+                        startActivity(univerisdad);
+                    }
+                });
                 /////COZUMEL///////
+                inst_partenon_cozumel.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","inst_partenon_cozumel");
+                        startActivity(univerisdad);
+                    }
+                });
+
                 /////BENITO JUÁREZ///////
+                upqroo_benito_juarez.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","upqroo_benito_juarez");
+                        startActivity(univerisdad);
+                    }
+                });
+                ut_benito_juarez.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","ut_benito_juarez");
+                        startActivity(univerisdad);
+                    }
+                });
+                uni_henbord_benito_juarez.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","uni_henbord_benito_juarez");
+                        startActivity(univerisdad);
+                    }
+                });
+                uni_azteca_benito_juarez.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","uni_azteca_benito_juarez");
+                        startActivity(univerisdad);
+                    }
+                });
+                inst_humanista_gestalt_benito_juarez.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","inst_humanista_gestalt_benito_juarez");
+                        startActivity(univerisdad);
+                    }
+                });
+                la_salle_benito_juarez.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","la_salle_benito_juarez");
+                        startActivity(univerisdad);
+                    }
+                });
+                centro_gestalt_benito_juarez.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","centro_gestalt_benito_juarez");
+                        startActivity(univerisdad);
+                    }
+                });
+                unisur_benito_juarez.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","unisur_benito_juarez");
+                        startActivity(univerisdad);
+                    }
+                });
+                instituto_sup_leyes_benito_juarez.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Do something in response to button click
+                        Intent univerisdad = new Intent( getActivity() , Uni_detalle.class);
+                        univerisdad.putExtra("uni","unisur_benito_juarez");
+                        startActivity(univerisdad);
+                    }
+                });
             }
         });
         return root;
