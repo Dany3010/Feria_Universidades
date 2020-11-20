e9695b0c3b72f031e2560ae0b3100a7621df67be
yosma <yosmarherrera000@gmail.com>
2020-11-19 19:47:43 -0500

    universidad detalle

--- app/src/main/java/com/example/feria_universidades/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/feria_universidades/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,27 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.feria_universidades.R;
 
 public class NotificationsFragment extends Fragment {
+    /////OTHON P. BLANCO////////
+Button itzm,cudv,unid_chetumal,uni_modelo_chetumal,uni_wozniak,uqroo_chetumal,ut_chetumal;
+
+    /////BACALAR////////
+Button uni_poli_bacalar,centro_regio_edu_bacalar;
+
+    /////FELIPE CARRILLO PUERTO////////
+    Button upp,inst_tec_felipe_c;
+
+    /////JOSE MARIA MORELOS///////
+Button uimqroo;
+
+    /////SOLIDARIDAD///////
+    Button UniversidadSur_soli,universidadriviera_soli,ut_rivieramaya_soli,unid_soli,esc_enfermeria_soli;
+
+    /////COZUMEL///////
+    Button inst_partenon_cozumel;
+
+    /////BENITO JUÁREZ///////
+    Button upqroo_benito_juarez,ut_benito_juarez,uni_henbord_benito_juarez,uni_azteca_benito_juarez,centro_gestalt_benito_juarez;
+    Button la_salle_benito_juarez,inst_humanista_gestalt_benito_juarez,unisur_benito_juarez,instituto_sup_leyes_benito_juarez;
 
     private NotificationsViewModel notificationsViewModel;
 
@@ -28,6 +50,13 @@ public class NotificationsFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+                /////OTHON P. BLANCO////////
+                ///////BACALAR/////////
+                /////FELIPE CARRILLO PUERTO////////
+                /////JOSE MARIA MORELOS///////
+                /////SOLIDARIDAD///////
+                /////COZUMEL///////
+                /////BENITO JUÁREZ///////
             }
         });
         return root;
