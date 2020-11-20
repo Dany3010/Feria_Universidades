6ed8095f28126fb9e4c6aa2f7aa390cfc9c9dc9d
Dany3010 <201800081@estudiantes.upqroo.edu.mx>
2020-11-20 14:17:02 -0500

    perfil usuario v1

--- app/src/main/java/com/example/feria_universidades/Login.java
+++ app/src/main/java/com/example/feria_universidades/Login.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +21,17 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,7 +42,9 @@ public class Login extends AppCompatActivity {
 
     EditText edtUsuario, edtPassword;
     Button btnLogin;
-    String usuario, password;
+    String usuario;
+    String password, nombre;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +71,7 @@ recuperarPreferencias();
             public void onResponse(String response) {
                if (!response.isEmpty()){
                    guardarPreferencias();
+                   new  ConsultarDatos().execute("https://feria-universidades2020.000webhostapp.com/android/perfil_usuario.php?codigo=" + edtUsuario.getText().toString());
                    Intent intent = new Intent(getApplicationContext(),Feria_universitaria.class);
                    startActivity(intent);
                    finish();
@@ -85,8 +101,10 @@ recuperarPreferencias();
     private void guardarPreferencias(){
         SharedPreferences preferences=getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor=preferences.edit();
+
         editor.putString("usuario",usuario);
         editor.putString("password",password);
+
         editor.putBoolean("sesion",true);
         editor.commit();
     }
@@ -94,5 +112,81 @@ recuperarPreferencias();
         SharedPreferences preferences=getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
         edtUsuario.setText(preferences.getString("usuario",""));
         edtPassword.setText(preferences.getString("password",""));
+    }
+
+
+    private class ConsultarDatos extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            JSONArray ja = null;
+            try {
+                ja = new JSONArray(result);
+                SharedPreferences preferences=getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor=preferences.edit();
+                editor.putString("nombre",(ja.getString(1)+" "+ja.getString(2)+" "+ja.getString(3)));
+                editor.putString("numero",(ja.getString(4)));
+                editor.putString("escuela",(ja.getString(5)));
+                editor.putString("promedio",(ja.getString(6)));
+                editor.commit();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        Log.i("URL",""+myurl);
+        myurl = myurl.replace(" ","%20");
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("respuesta", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
     }
 }
