a655b714e040dace208d91977ebd131c593f2ea4
Dany3010 <201800081@estudiantes.upqroo.edu.mx>
2020-11-29 21:43:52 -0500

    cambios finales

--- app/src/main/java/com/example/feria_universidades/MainActivity2.java
+++ app/src/main/java/com/example/feria_universidades/MainActivity2.java
@@ -3,19 +3,30 @@ package com.example.feria_universidades;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
-    private Button home;
-    private Button uni;
+     Button home , fin;
+    Button uni;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_vocacional);
         WebView myWebView = (WebView) findViewById(R.id.test);
         myWebView.loadUrl("https://www.elegircarrera.net/test-vocacional.aspx");
+        fin=findViewById(R.id.button2);
 
+        fin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myWebView.loadUrl("https://www.elegircarrera.net/Test/ResultadoPrueba.aspx");
+            }
+        });
 
     }
 
